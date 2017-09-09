4547a184cc0fd8e8748082ca09047e4a761d1fdc
prajil <tpprajilkottur@gmail.com>
2017-09-09 15:11:12 +0530

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+cabs = Cab.create([{number: 'KL 15 6565', contact_number: '1234567890',
+  curr_lat: 11.2587531, curr_long: 75.78041},
+  { number: 'KL 15 6566', contact_number: '1244567890',
+    curr_lat: 11.2587531, curr_long: 75.78041},
+  {number: 'KL 15 6567', contact_number: '1234567899', pink: true,
+    curr_lat: 11.2587531, curr_long: 75.78041},
+  {number: 'KL 15 6267', contact_number: '1234567499', pink: true,
+    curr_lat: 9.9312328, curr_long: 76.26730409999999},
+  {number: 'KL 15 1567', contact_number: '1234563899',
+    curr_lat: 12.9715987, curr_long: 77.5945627} ])
